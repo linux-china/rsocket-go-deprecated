beea3b8a1850b71fef2041991dec54c43fd92478
Libing Chen <libing.chen@gmail.com>
2019-01-11 19:44:34 -0800

    polishing

--- test/client_test.go
+++ test/client_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"bytes"
 	"github.com/linux-china/rsocket-go/rsocket"
 	"github.com/reactivex/rxgo/handlers"
 	"github.com/reactivex/rxgo/observer"
@@ -8,7 +9,7 @@ import (
 )
 
 func TestRSocketRequestResponse(t *testing.T) {
-	var payload = rsocket.Payload{Metadata: []byte("metadata"), Data: []byte("data")}
+	var payload = rsocket.Payload{Metadata: bytes.NewBufferString("ip:192.168.0.2"), Data: bytes.NewBufferString("Hello World!")}
 	var client = rsocket.Connect().Transport("tcp://127.0.0.1:42252").Start()
 	handler := observer.New(handlers.NextFunc(func(payload interface{}) {
 		println(payload)
