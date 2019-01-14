f69bcdd1f96ba2556d9a05496534a82ad1045aa7
Libing Chen <libing.chen@gmail.com>
2019-01-14 11:22:50 -0800

    move code to connection

--- rsocket/handlers.go
+++ rsocket/handlers.go
@@ -4,19 +4,6 @@ import (
 	"github.com/reactivex/rxgo/observable"
 )
 
-type Availability interface {
-	Availability() float64
-}
-
-type Closeable interface {
-	OnClose() observable.Observable
-}
-
-type Disposable interface {
-	Dispose()
-	IsDisposed() bool
-}
-
 type (
 	// accept for setup function
 	RSocketAccept func(payload Payload, socket RSocket) RSocket
