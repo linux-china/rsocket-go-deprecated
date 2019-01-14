dfae2c7da60280323e447d7ee00bb8eeb87f7cce
Libing Chen <libing.chen@gmail.com>
2019-01-14 11:50:47 -0800

    add TestAtomicCounter

--- test/bytes_test.go
+++ test/bytes_test.go
@@ -2,10 +2,18 @@ package test
 
 import (
 	"bytes"
+	"sync/atomic"
 	"testing"
 )
+
+var counter uint32 = 0
 
 func TestBytesBuffer(t *testing.T) {
 	var content = bytes.NewBufferString("demo")
 	print(content.String())
+}
+
+func TestAtomicCounter(t *testing.T) {
+	atomic.AddUint32(&counter, 1)
+	print(counter)
 }
