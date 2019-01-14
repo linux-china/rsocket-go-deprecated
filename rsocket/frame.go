88cef18d5620c3d487fac5d70905b42743190e8d
Libing Chen <libing.chen@gmail.com>
2019-01-14 11:35:16 -0800

    add fields and methods for Frame

--- rsocket/frame.go
+++ rsocket/frame.go
@@ -1,4 +1,20 @@
 package rsocket
 
 type Frame struct {
+	streamId  int32
+	FrameType byte
+	Flags     int16
+	Payload
+}
+
+func (frame *Frame) HasMetadata() bool {
+	return true
+}
+
+func NewSetupFrame(metadataMimeType string, dataMimeType string, payload Payload) Frame {
+	return Frame{}
+}
+
+func NewRequestFrame(streamId int32, payload Payload, initialRequestN int32) Frame {
+	return Frame{}
 }
